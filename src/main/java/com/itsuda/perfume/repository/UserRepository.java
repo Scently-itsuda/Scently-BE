19432b97d7ec8c5e616d458160dc35097aa2a519
nicolao00 <nicolao00@naver.com>
2025-01-22 13:11:53 +0900

    fix: 잘못된 타입 변경

--- src/main/java/com/itsuda/perfume/repository/UserRepository.java
+++ src/main/java/com/itsuda/perfume/repository/UserRepository.java
@@ -16,7 +16,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     @Query(value = "SELECT u.id as id, u.role as role" +
             " FROM User u WHERE u.serialId = :userId")
-    Optional<UserSecurityForm> findUserIdAndRoleBySerialId(@Param("userId") Long userId);
+    Optional<UserSecurityForm> findUserIdAndRoleBySerialId(@Param("userId") String userId);
 
     Optional<UserSecurityForm> findByIdAndRefreshTokenIsNotNull(Long id);
 

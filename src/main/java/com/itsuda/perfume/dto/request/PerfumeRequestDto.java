b06d81723c30ebf2cecbcae4d92ef8c67933a9b7
nicolao00 <nicolao00@naver.com>
2025-01-25 17:36:13 +0900

    feat: 클라이언트를 고려해 Swagger 예시값 추가

--- src/main/java/com/itsuda/perfume/dto/request/PerfumeRequestDto.java
+++ src/main/java/com/itsuda/perfume/dto/request/PerfumeRequestDto.java
@@ -5,6 +5,7 @@ import com.itsuda.perfume.domain.type.CountryType;
 import com.itsuda.perfume.domain.type.GenderType;
 import com.itsuda.perfume.domain.type.PotentialType;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,10 +16,21 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class PerfumeRequestDto {
+    @Schema(description = "가격", example = "150000", minimum = "0")
     private Integer price;
+
+    @Schema(description = "성별", example = "UNKNOWN", allowableValues = {"MALE", "FEMALE", "UNISEX", "UNKNOWN"})
     private GenderType gender;
+
+    @Schema(description = "향 계열", example = "만다린", examples = {"만다린", "바닐라", "바이올렛", "베르가못", "..."})
     private String accord;
+
+    @Schema(description = "향의 강도", example = "EDT", allowableValues = {"EDP", "EDT", "EDC", "PERFUME"})
     private PotentialType potential;
+
+    @Schema(description = "브랜드", example = "CHANEL", examples = {"CHANEL", "DIOR", "GUCCI", "JO MALONE", "CREED", "BYREDO", "..."})
     private BrandType brand;
+
+    @Schema(description = "원산지", example = "FRANCE", examples = {"FRANCE", "ITALY", "USA", "..."})
     private CountryType country;
 }

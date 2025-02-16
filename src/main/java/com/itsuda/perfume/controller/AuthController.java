880a50d3d594e0e7c2c693ae48d4b83b4c48ce64
nicolao00 <nicolao00@naver.com>
2025-02-16 21:45:48 +0900

    fix: 소셜 로그인 사용자 정보 등록 API에도 닉네임 검증 추가

--- src/main/java/com/itsuda/perfume/controller/AuthController.java
+++ src/main/java/com/itsuda/perfume/controller/AuthController.java
@@ -14,9 +14,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,7 +29,7 @@ public class AuthController {
 
     //소셜 로그인 사용자 정보 등록
     @PatchMapping("/resister")
-    public ResponseDto<?> registerUserInfo(@Parameter(hidden = true) @UserId Long id, @RequestBody UserResisterDto requestDto) {
+    public ResponseDto<?> registerUserInfo(@Parameter(hidden = true) @UserId Long id, @Valid @RequestBody UserResisterDto requestDto) {
         return new ResponseDto<>(authService.registerUserInfo(id, requestDto));
     }
 

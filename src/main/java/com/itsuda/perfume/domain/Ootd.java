38c1657590e9b6cc1daad21f73eb320b55450b66
prefer52 <prefer0502@gmail.com>
2025-02-26 17:07:03 +0900

    feat: 댓글, 좋아요, 태그를 제외한 OOTD 엔티티 설정 추가

---
+++ src/main/java/com/itsuda/perfume/domain/Ootd.java
@@ -0,0 +1,67 @@
+package com.itsuda.perfume.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Ootd extends ModifiableBaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    @ColumnDefault("0")
+    private int likeCount;
+
+    @Column(nullable = false)
+    @ColumnDefault("0")
+    private int commentCount;
+
+    @Column(nullable = false)
+    private int volume;
+
+    @Column(nullable = false)
+    private String content;
+
+    // ------------------------ 관계 설정 ----------------------------
+
+    @OneToMany(mappedBy = "ootd")
+    private List<OotdImage> ootdImages = new ArrayList<>();
+
+    @ManyToOne
+    @JoinColumn(name = "perfume_id")
+    private Perfume perfume;
+
+    /*
+    Todo - 태그 연관 관계 설정
+    Todo - 댓글 연관 관계 설정
+    Todo - 좋아요 연관 관계 설정
+     */
+
+    @Builder
+    private Ootd(int likeCount, int commentCount, int volume, String content, List<OotdImage> ootdImages, Perfume perfume) {
+        this.likeCount = likeCount;
+        this.commentCount = commentCount;
+        this.volume = volume;
+        this.content = content;
+        this.ootdImages = ootdImages;
+        this.perfume = perfume;
+    }
+}
---
+++ src/main/java/com/itsuda/perfume/domain/OotdImage.java
@@ -0,0 +1,45 @@
+package com.itsuda.perfume.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class OotdImage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String originName;
+
+    @Column(nullable = false)
+    private String saveName;
+
+    @Column(nullable = false)
+    private int sequence;
+
+    // ------------------------ 관계 설정 ----------------------------
+
+    @ManyToOne
+    @JoinColumn(name = "ootd_id", nullable = false)
+    private Ootd ootd;
+
+    @Builder
+    private OotdImage(String originName, String saveName, int sequence) {
+        this.originName = originName;
+        this.saveName = saveName;
+        this.sequence = sequence;
+    }
+}

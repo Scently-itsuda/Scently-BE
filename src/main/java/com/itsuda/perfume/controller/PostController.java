c9ccf16b1dee8d8a6d879e0a82f6c54b4a7da53d
prefer52 <prefer0502@gmail.com>
2025-03-18 23:44:16 +0900

    feat: 컨트롤러 레이어 구현 및 테스트

---
+++ src/main/java/com/itsuda/perfume/controller/PostController.java
@@ -0,0 +1,28 @@
+package com.itsuda.perfume.controller;
+
+import com.itsuda.perfume.domain.type.PostOrderType;
+import com.itsuda.perfume.dto.response.post.PostMainDto;
+import com.itsuda.perfume.exception.ResponseDto;
+import com.itsuda.perfume.service.PostService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/posts")
+@Tag(name = "Post", description = "자유게시판 관련 API")
+public class PostController {
+
+    private final PostService postService;
+
+    @GetMapping
+    public ResponseDto<PostMainDto> getPosts(
+            @RequestParam(required = false, defaultValue = "NEWEST") PostOrderType order,
+            @RequestParam int page, @RequestParam int size) {
+        return new ResponseDto<>(postService.getPostsByOrderType(page, size, order));
+    }
+}
---
+++ src/test/java/com/itsuda/perfume/controller/PostControllerTest.java
@@ -0,0 +1,48 @@
+package com.itsuda.perfume.controller;
+
+import com.itsuda.perfume.domain.type.PostOrderType;
+import com.itsuda.perfume.dto.response.post.PostMainDto;
+import com.itsuda.perfume.service.PostService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(PostController.class)
+@WithMockUser
+class PostControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private PostService postService;
+
+    @DisplayName("자유게시판의 게시글 목록들을 조회한다.")
+    @Test
+    void test() throws Exception {
+        // given
+        PostMainDto result = new PostMainDto(null, null);
+        Mockito.when(postService.getPostsByOrderType(anyInt(), anyInt(), any(PostOrderType.class)))
+                .thenReturn(result);
+
+        // when // then
+        mockMvc.perform(
+                        MockMvcRequestBuilders.get("/api/v1/posts")
+                                .queryParam("page", "0")
+                                .queryParam("size", "3")
+                ).andExpect(status().isOk())
+                .andExpect(jsonPath("$.result").value("1200"));
+
+    }
+}

7e1570c1f16c3ba8f8a969f0580d48355433942d
prefer52 <prefer0502@gmail.com>
2025-08-31 18:15:21 +0900

    refactor: OotdService 코드 리팩토링

--- src/main/java/com/itsuda/perfume/service/OotdService.java
+++ src/main/java/com/itsuda/perfume/service/OotdService.java
@@ -11,6 +11,7 @@ import com.itsuda.perfume.domain.User;
 import com.itsuda.perfume.domain.UserFcmToken;
 import com.itsuda.perfume.domain.UserLikeComment;
 import com.itsuda.perfume.domain.UserLikeOotd;
+import com.itsuda.perfume.domain.type.NotificationType;
 import com.itsuda.perfume.domain.type.OotdOrderType;
 import com.itsuda.perfume.dto.response.PageInfoDto;
 import com.itsuda.perfume.dto.response.ootd.CommentsDto;
@@ -63,7 +64,9 @@ public class OotdService {
 
     private final OotdPerfumeJdbcTemplateRepository ootdPerfumeJdbcTemplateRepository;
     private final OotdImageJdbcTemplateRepository ootdImageJdbcTemplateRepository;
+    private final OotdTagJdbcTemplateRepository ootdTagJdbcTemplateRepository;
     private final TagJdbcTemplateRepository tagJdbcTemplateRepository;
+
     private final UserLikeCommentRepository userLikeCommentRepository;
     private final UserLikeOotdRepository userLikeOotdRepository;
     private final UserFcmTokenRepository userFcmTokenRepository;
@@ -74,36 +77,35 @@ public class OotdService {
     private final OotdRepository ootdRepository;
     private final UserRepository userRepository;
     private final FcmService fcmService;
+
     private final S3Util s3Util;
-    private final OotdTagJdbcTemplateRepository ootdTagJdbcTemplateRepository;
 
     @Value("${cloud.aws.s3.save-path.ootd-image}")
     private String OOTD_IMAGE_SAVE_PATH;
 
     public OotdMainDto getOotdThumbnailsByOrderType(int page, int size, OotdOrderType ootdOrderType, Long userId) {
-        Pageable pageable = PageRequest.of(page, size,
-                switch (ootdOrderType) {
-                    case NEWEST_DESCENDING -> Sort.by("created_at").descending();
-                    case NEWEST_ASCENDING -> Sort.by("created_at").ascending();
-                    case POPULAR_DESCENDING -> Sort.by("like_count").descending();
-                    case POPULAR_ASCENDING -> Sort.by("like_count").ascending();
-                });
+        Pageable pageable = PageRequest.of(page, size, switch (ootdOrderType) {
+            case NEWEST_DESCENDING -> Sort.by("created_at").descending();
+            case NEWEST_ASCENDING -> Sort.by("created_at").ascending();
+            case POPULAR_DESCENDING -> Sort.by("like_count").descending();
+            case POPULAR_ASCENDING -> Sort.by("like_count").ascending();
+        });
 
         return OotdMainDto.from(ootdRepository.findAllIncludingUserLiked(pageable, userId));
     }
 
     @Transactional
     public CreatedOotdDto createOotd(Long userId, String content, List<String> tagNames, int volume,
-                                     List<Long> perfumeId, List<MultipartFile> images) {
+                                     List<Long> perfumeIds, List<MultipartFile> images) {
         User user = userRepository.findById(userId).orElseThrow(() -> new RestApiException(NOT_FOUND_USER));
-        List<Perfume> perfumes = perfumeRepository.findAllByIdIn(perfumeId);
+        List<Perfume> perfumes = perfumeRepository.findAllByIdIn(perfumeIds);
 
-        Ootd ootd = ootdRepository.save(Ootd.builder().likeCount(0).volume(volume).user(user).content(content).build());
+        Ootd ootd = ootdRepository.save(Ootd.builder().volume(volume).user(user).content(content).build());
 
         List<OotdImage> ootdImages = IntStream.range(0, images.size()).mapToObj(index -> OotdImage.builder()
                 .ootd(ootd)
                 .originName(images.get(index).getOriginalFilename())
-                .saveName(UUID.randomUUID() + FileUtil.getFileExtensionWithDot(images.get(index).getOriginalFilename()))
+                .saveName(FileUtil.getRandomFileName(images.get(index).getOriginalFilename()))
                 .sequence(index).build()).toList();
         ootdImageJdbcTemplateRepository.batchInsert(ootdImages);
 
@@ -116,16 +118,14 @@ public class OotdService {
         List<Long> tagIds = LongStream.rangeClosed(firstInsertedTagId - tags.size() + 1, firstInsertedTagId).boxed().toList();
         ootdTagJdbcTemplateRepository.batchInsert(tagIds, ootd);
 
-        s3Util.uploadFiles(FileUtil.getFileBytes(images), OOTD_IMAGE_SAVE_PATH, ootdImages.stream().map(OotdImage::getSaveName).toList(), FileUtil.getContentTypes(images));
+        s3Util.uploadFiles(FileUtil.getFileBytes(images), OOTD_IMAGE_SAVE_PATH,
+                ootdImages.stream().map(OotdImage::getSaveName).toList(), FileUtil.getContentTypes(images));
 
         return new CreatedOotdDto(ootd.getId());
     }
 
     public OotdDetailDto getOotdDetailByOotdId(Long ootdId, Long userId) {
-        Ootd ootd = ootdRepository.findByIdWithOotdImages(ootdId).orElseThrow(() -> new RestApiException(NOT_FOUND_OOTD));
-        if (Optional.ofNullable(ootd.getDeletedAt()).isPresent()) {
-            throw new RestApiException(DELETED_OOTD);
-        }
+        Ootd ootd = validateOotd(ootdRepository.findByIdWithOotdImages(ootdId));
 
         List<OotdPerfume> ootdPerfumes = ootdPerfumeRepository.findByOotd(ootd);
         boolean isLiked = Optional.ofNullable(userId).map(ootdUserId -> {
@@ -138,11 +138,7 @@ public class OotdService {
 
     @Transactional
     public void deleteOotdByOotdId(Long ootdId, Long userId) {
-        Ootd ootd = ootdRepository.findById(ootdId).orElseThrow(() -> new RestApiException(NOT_FOUND_OOTD));
-        if (Optional.ofNullable(ootd.getDeletedAt()).isPresent()) {
-            throw new RestApiException(DELETED_OOTD);
-        }
-
+        Ootd ootd = validateOotd(ootdRepository.findById(ootdId));
         User user = userRepository.findById(userId).orElseThrow(() -> new RestApiException(NOT_FOUND_USER));
         if (!ootd.getUser().getId().equals(user.getId())) {
             throw new RestApiException(ONLY_OOTD_OWNER_DELETE);
@@ -152,10 +148,7 @@ public class OotdService {
     }
 
     public CommentsDto getCommentsByOotdId(Long ootdId) {
-        Ootd ootd = ootdRepository.findById(ootdId).orElseThrow(() -> new RestApiException(NOT_FOUND_OOTD));
-        if (Optional.ofNullable(ootd.getDeletedAt()).isPresent()) {
-            throw new RestApiException(DELETED_OOTD);
-        }
+        Ootd ootd = validateOotd(ootdRepository.findById(ootdId));
         List<Comment> comments = commentRepository.findAllByOotdAndParentCommentIsNullAndDeletedAtIsNull(ootd);
 
         return CommentsDto.from(comments);
@@ -164,11 +157,7 @@ public class OotdService {
     @Async
     @Transactional
     public void sendLikeToOotd(Long ootdId, Long userId) {
-        Ootd ootd = ootdRepository.findById(ootdId).orElseThrow(() -> new RestApiException(NOT_FOUND_OOTD));
-        if (Optional.ofNullable(ootd.getDeletedAt()).isPresent()) {
-            throw new RestApiException(DELETED_OOTD);
-        }
-
+        Ootd ootd = validateOotd(ootdRepository.findById(ootdId));
         User user = userRepository.findById(userId).orElseThrow(() -> new RestApiException(NOT_FOUND_USER));
         Optional<UserFcmToken> userFcmToken = userFcmTokenRepository.findByUser(ootd.getUser());
 
@@ -181,27 +170,12 @@ public class OotdService {
 
         userLikeOotdRepository.save(UserLikeOotd.builder().ootd(ootd).user(user).build());
         ootd.increaseLikeCount();
-        userFcmToken.ifPresent(fcmToken -> {
-                    Notification notification = notificationRepository.save(
-                            Notification.builder()
-                                    .title(user.getNickname() + "님이 회원님의 OOTD를 추천합니다.")
-                                    .bodyMessage(ootd.getContent())
-                                    .notificationSender(user)
-                                    .notificationReceiver(ootd.getUser())
-                                    .targetId(ootd.getId())
-                                    .notificationType(OOTD_LIKE)
-                                    .build());
-                    fcmService.sendFCMMessage(notification.getTitle(), notification.getBodyMessage(), fcmToken.getFcmToken());
-                }
-        );
+        sendFCMMessage(userFcmToken, user.getNickname() + "님이 회원님의 OOTD를 추천합니다.", ootd.getContent(), user, ootd.getUser(), ootd.getId(), OOTD_LIKE);
     }
 
     @Transactional
     public OotdCommentDto writeCommentToOotd(Long ootdId, Long userId, Long parentCommentId, String content) {
-        Ootd ootd = ootdRepository.findByIdWithUser(ootdId).orElseThrow(() -> new RestApiException(NOT_FOUND_OOTD));
-        if (Optional.ofNullable(ootd.getDeletedAt()).isPresent()) {
-            throw new RestApiException(DELETED_OOTD);
-        }
+        Ootd ootd = validateOotd(ootdRepository.findByIdWithUser(ootdId));
         User user = userRepository.findById(userId).orElseThrow(() -> new RestApiException(NOT_FOUND_USER));
         Optional<UserFcmToken> userFcmToken = userFcmTokenRepository.findByUser(ootd.getUser());
         Optional<Comment> parentComment = Optional.ofNullable(parentCommentId).flatMap(commentRepository::findById);
@@ -214,28 +188,13 @@ public class OotdService {
                 .post(null)
                 .user(user)
                 .build());
-
-        userFcmToken.ifPresent(fcmToken -> {
-                    Notification notification = notificationRepository.save(Notification.builder()
-                            .title(user.getNickname() + "님이 " + ootd.getUser() + "님의 게시물에 댓글을 남겼습니다.")
-                            .bodyMessage(comment.getContent())
-                            .notificationSender(user)
-                            .notificationReceiver(comment.getUser())
-                            .targetId(ootd.getId())
-                            .notificationType(OOTD_COMMENT)
-                            .build());
-                    fcmService.sendFCMMessage(notification.getTitle(), notification.getBodyMessage(), fcmToken.getFcmToken());
-                }
-        );
+        sendFCMMessage(userFcmToken, user.getNickname() + "님이 " + ootd.getUser() + "님의 게시물에 댓글을 남겼습니다.", comment.getContent(), user, comment.getUser(), ootd.getId(), OOTD_COMMENT);
         return new OotdCommentDto(comment.getId());
     }
 
     @Transactional
     public void deleteOotdComment(Long userId, Long commentId) {
-        Comment comment = commentRepository.findByIdWithUser(commentId).orElseThrow(() -> new RestApiException(NOT_FOUND_COMMENT));
-        if (Optional.ofNullable(comment.getDeletedAt()).isPresent()) {
-            throw new RestApiException(DELETED_COMMENT);
-        }
+        Comment comment = validateComment(commentRepository.findByIdWithUser(commentId));
         User user = userRepository.findById(userId).orElseThrow(() -> new RestApiException(NOT_FOUND_USER));
         if (!comment.getUser().getId().equals(user.getId())) {
             throw new RestApiException(ONLY_COMMENT_OWNER_DELETE);
@@ -246,10 +205,7 @@ public class OotdService {
 
     @Transactional
     public void sendLikeToOotdComment(Long userId, Long commentId) {
-        Comment comment = commentRepository.findById(commentId).orElseThrow(() -> new RestApiException(NOT_FOUND_COMMENT));
-        if (Optional.ofNullable(comment.getDeletedAt()).isPresent()) {
-            throw new RestApiException(DELETED_COMMENT);
-        }
+        Comment comment = validateComment(commentRepository.findById(commentId));
         User user = userRepository.findById(userId).orElseThrow(() -> new RestApiException(NOT_FOUND_USER));
 
         Optional<UserLikeComment> userLikeComment = userLikeCommentRepository.findByCommentAndUser(comment, user);
@@ -277,5 +233,35 @@ public class OotdService {
                 });
         Page<UserLikeOotdInfo> userLikeOotdInfos = ootdRepository.findAllUserLikeByUser(pageable, userId);
         return UserLikeOotdsDto.from(userLikeOotdInfos.getContent(), PageInfoDto.from(userLikeOotdInfos));
+    }
+
+    private Ootd validateOotd(Optional<Ootd> optionalOotd) {
+        Ootd ootd = optionalOotd.orElseThrow(() -> new RestApiException(NOT_FOUND_OOTD));
+        if (Optional.ofNullable(ootd.getDeletedAt()).isPresent()) {
+            throw new RestApiException(DELETED_OOTD);
+        }
+        return ootd;
+    }
+
+    private Comment validateComment(Optional<Comment> optionalComment) {
+        Comment comment = optionalComment.orElseThrow(() -> new RestApiException(NOT_FOUND_COMMENT));
+        if (Optional.ofNullable(comment.getDeletedAt()).isPresent()) {
+            throw new RestApiException(DELETED_COMMENT);
+        }
+        return comment;
+    }
+
+    private void sendFCMMessage(Optional<UserFcmToken> userFcmToken, String title, String bodyMessage, User sender, User receiver, Long targetId, NotificationType notificationType) {
+        userFcmToken.ifPresent(fcmToken -> {
+                    Notification notification = notificationRepository.save(Notification.builder()
+                            .title(title)
+                            .bodyMessage(bodyMessage)
+                            .notificationSender(sender)
+                            .notificationReceiver(receiver)
+                            .targetId(targetId)
+                            .notificationType(notificationType).build());
+                    fcmService.sendFCMMessage(notification.getTitle(), notification.getBodyMessage(), fcmToken.getFcmToken());
+                }
+        );
     }
 }
--- src/main/java/com/itsuda/perfume/util/FileUtil.java
+++ src/main/java/com/itsuda/perfume/util/FileUtil.java
@@ -5,10 +5,19 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.UUID;
 
 import static com.itsuda.perfume.exception.ErrorCode.*;
 
 public class FileUtil {
+
+    public static String getRandomFileName(String fileName) {
+        return UUID.randomUUID() + getFileExtensionWithDot(fileName);
+    }
+
+    public static String getFileExtension(String fileName) {
+        return fileName.substring(fileName.lastIndexOf('.') + 1);
+    }
 
     public static String getFileExtensionWithDot(String fileName) {
         return fileName.substring(fileName.lastIndexOf('.'));
